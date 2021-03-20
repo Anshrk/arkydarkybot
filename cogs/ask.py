a17cf7ac4d0b8107f230e6d9e0fe3ecb411b6b7b
Anshrk <anshrk.13@gmail.com>
2021-03-20 19:44:11 +0530

    more more more

--- cogs/ask.py
+++ cogs/ask.py
@@ -16,19 +16,19 @@ class AI(commands.Cog):
         self.client = client
     
     @commands.Command
-    def ask(self, ctx, question, chat_log=None):
+    async def ask(self, ctx, *,  question, chat_log=None):
         """ASK IT SHIT"""
         if chat_log == None:
             chat_log = start_chat_log
         
         prompt = f'{chat_log} Human: {question}\nAI:'
-        response = completion.create(
+        response = await completion.create(
             prompt=prompt, engine="davince", stop=['\nHuman'], temperature=0.9,
             top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
             max_tokens=150
         )
         answer=response.choice[0].text.strip()
-        ctx.send(answer)
+        await ctx.send(answer)
 
 
 
