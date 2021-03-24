f3fd061b2bac2155627b343e6b7df6c5d494737a
Anshrk <anshrk.13@gmail.com>
2021-03-25 00:15:47 +0530

    first

--- cogs/ask.py
+++ cogs/ask.py
@@ -11,27 +11,36 @@ start_chat_log = '''Human: Hello, who are you?
 AI: I am doing great. How can I help you today?
 '''
 
-class AI(commands.Cog):
+
+def ask(question, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    prompt = f'{chat_log}Human: {question}\nAI:'
+    response = completion.create(
+        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=150)
+    answer = response.choices[0].text.strip()
+    return answer
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    return f'{chat_log}Human: {question}\nAI: {answer}\n'
+
+
+class AskCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.chat_log = 'Hi, I am human, nice to meet you.\n'
     
-    @commands.Command
-    async def ask(self, ctx, *,  question, chat_log=None):
-        """ASK IT SHIT"""
-        if chat_log == None:
-            chat_log = start_chat_log
-        
-        prompt = f'{chat_log} Human: {question}\nAI:'
-        response = await completion.create(
-            prompt=prompt, engine="davince", stop=['\nHuman'], temperature=0.9,
-            top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-            max_tokens=150
-        )
-        answer=response.choice[0].text.strip()
-        await ctx.send(answer)
+    @commands.command(aliases=["ask"])
+    async def askAi(self, ctx, *, arg):
 
+        question = str(arg)
+        answer = ask(question=question, chat_log=self.chat_log)
+        await ctx.send(f'AI: {answer}')
 
 
 def setup(client):
-    """Set up all the cogs."""
-    client.add_cog(AI(client))
+    client.add_cog(AskCommands(client))
